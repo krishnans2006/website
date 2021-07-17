d7fbe69fd6b1151d2a3a1e5227379f7207b396bd
Krishnan Shankar <krishnans2006@gmail.com>
2021-07-16 22:40:00 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for, send_file
+
+from data import get_projects
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return redirect("/portfolio")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html", projects=[[p.to_dict(), p.id] for p in get_projects()], desc="Test!")
+
+
+@app.route("/resume")
+def resume():
+    return send_file("Resume.pdf", as_attachment=True)
+
+
+@app.errorhandler(404)
+def error404(e):
+    return render_template("error.html", error=404, exception=e)
+
+
+@app.errorhandler(500)
+def error500(e):
+    return render_template("error.html", error=500, exception=e)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ data.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("creds.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+def get_projects():
+    return list(db.collection("Projects")
+                .order_by("Winner", direction=firestore.Query.DESCENDING)
+                .order_by("Pinned", direction=firestore.Query.DESCENDING)
+                .order_by("Order", direction=firestore.Query.DESCENDING)
+                .stream()
+                )
